816538f6aff20e1d09ef10b141f8ba623178da8b
Sazmina <100283466+Sazmina@users.noreply.github.com>
2022-02-24 13:14:54 +0000

    Update Characters.py

--- Characters.py
+++ Characters.py
@@ -1,3 +1,27 @@
+import simplegui
+
+#Constants for the size of the canvas (temporarily set to 800 x 600 - we can change this)
+WIDTH = 800
+HEIGHT = 600
+
+#This variable indicates whether the game has started
+started = False
+
+#This variable stores the position of a mouse click
+pos = ""
+
+#This class handles the information of image objects, more attributes will be added as we code more image objects
+class ImageInfo:
+    def __init__(self, center, size):
+        self.center = center
+        self.size = size
+
+    def getCenter(self):
+        return self.center
+
+    def getSize(self):
+        return self.size
+
 #This class stores basic information about characters (Their attack, defence and movement) As these stats do not change between characters of the same type
 class Classes:
     def __init__(self, name, attack, defence, movement):
@@ -39,9 +63,8 @@ class Character:
         self.health = self.health
         #I will write this out in more detail when I begin work on the fight screen, for now, do not mind this function, it is simply a placeholder and an indication of how fights will begin
 
-
-
-
+def temp():
+    pass
 
 #Order of stats is as follows (ATTACK, DEFENCE, MOVEMENT)
 Warrior = Classes("Warrior", 3, 2, 3)
@@ -54,6 +77,46 @@ King = Classes ("King", 4, 2, 3)
 RedTeamWarrior1 = (Warrior, 10, (0,0))
 #A basic example of how a character is created, his class is referred to, health is set, and position on the map is set
 
+#Replace this image with better graphics
+splashImage = simplegui.load_image("https://i.imgur.com/joIcLFR.png")
+
+#Splash screen menu info
+splashInfo = ImageInfo([350, 200], [700, 400])
+
+#Handler for a mouse click
+def click(pos):
+    global started
+
+    if (not started):
+        started = True
+    
+#Handler to draw on canvas
+def draw(canvas):
+    global started
+    
+    if not started:
+        # Drawing the splash screen on the canvas
+        # When replacing the 
+        canvas.draw_image(splashImage, splashInfo.getCenter(), 
+                          splashInfo.getSize(), [WIDTH/2, HEIGHT/2], 
+                          splashInfo.getSize())
+
+    canvas.draw_text("", [WIDTH/2,HEIGHT/2], 48, "Red")
+
+#Create a frame and assign callbacks to event handlers
+frame = simplegui.create_frame("Home", WIDTH, HEIGHT)
+
+#Register drawing handler
+frame.set_draw_handler(draw)
+
+#Register handler for mouse click events 
+frame.set_mouseclick_handler(click)
+
+#We will only keep this button if we decide to implement AI
+frame.add_button("Play with computer", temp, 140)
+
+#Start the frame animation
+frame.start()
 
 #This is not very advanced code, but serves as a good start to the project as the very basics of the character class are defined, these will be added to and further developed later.
 #For now, this should be enough to get started, feel free to add to these classes, but remember to add comments on GitHub to explain what you have updated
